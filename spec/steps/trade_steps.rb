3cd01d423942c7c34cb4436040332eb7fb686588
vivit <kazyu.vanilla@gmail.com>
2015-02-19 01:26:25 +0900

    Guardとterminal-notifierを導入

--- spec/steps/trade_steps.rb
+++ spec/steps/trade_steps.rb
@@ -1,5 +1,9 @@
 # encoding: utf-8
 
-step %(:textと表示されている) do |text|
+step %(:textと表示) do |text|
+  expect(page).to have_content(text)
+end
+
+step %(:textを表示) do |text|
   expect(page).to have_content(text)
 end
